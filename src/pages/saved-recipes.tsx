67f6c96b17b902e76f1c0cfd4872078fd6c22bd3
Emma Fattori <emma@applydigital.co>
2023-05-25 20:25:19 -0400

    Moving folder structures around, creating saved-recipes page

---
+++ src/pages/saved-recipes.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header/header"
+import "../globals.css"
+export default function SavedRecipes() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
+      This is the saved recipes pages
+    </main>
+  )
+}
