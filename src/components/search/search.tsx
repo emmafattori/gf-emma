0a6244369a6ae8968cdec45e36c2b86fc8dd307c
Emma Fattori <emma@applydigital.co>
2023-05-27 13:14:28 -0400

    Adding in search component boilerplate

---
+++ src/components/search/search.tsx
@@ -0,0 +1,9 @@
+export const Search = () => {
+  return (
+    <div>
+      <label htmlFor="search">Search for a new recipe</label>
+      <input id="search"></input>
+      <div className="">Results:</div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { Search } from "@/components/search/search"
 import "../globals.css"
 import { Header } from "@/components/header/header"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className=" min-h-screen p-5">
       <Header />
+      <Search />
     </main>
   )
 }
