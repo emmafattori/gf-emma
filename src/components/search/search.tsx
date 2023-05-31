101f51ea822c19efd51749deae4edb802332add3
Emma Fattori <emma@applydigital.co>
2023-05-30 22:12:04 -0400

    Adding fetch request

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,109 +1,27 @@
-import { MouseEvent } from "react"
-
-// export interface Response {
-//   from: 0
-//   to: 0
-//   count: 0
-//   _links: {
-//     self: {
-//       href: "string"
-//       title: "string"
-//     }
-//     next: {
-//       href: "string"
-//       title: "string"
-//     }
-//   }
-//   hits: [
-//     {
-//       recipe: {
-//         uri: "string"
-//         label: "string"
-//         image: "string"
-//         images: {
-//           THUMBNAIL: {
-//             url: "string"
-//             width: 0
-//             height: 0
-//           }
-//           SMALL: {
-//             url: "string"
-//             width: 0
-//             height: 0
-//           }
-//           REGULAR: {
-//             url: "string"
-//             width: 0
-//             height: 0
-//           }
-//           LARGE: {
-//             url: "string"
-//             width: 0
-//             height: 0
-//           }
-//         }
-//         source: "string"
-//         url: "string"
-//         shareAs: "string"
-//         yield: 0
-//         dietLabels: ["string"]
-//         healthLabels: ["string"]
-//         cautions: ["string"]
-//         ingredientLines: ["string"]
-//         ingredients: [
-//           {
-//             text: "string"
-//             quantity: 0
-//             measure: "string"
-//             food: "string"
-//             weight: 0
-//             foodId: "string"
-//           }
-//         ]
-//         calories: 0
-//         glycemicIndex: 0
-//         totalCO2Emissions: 0
-//         co2EmissionsClass: "A+"
-//         totalWeight: 0
-//         cuisineType: ["string"]
-//         mealType: ["string"]
-//         dishType: ["string"]
-//         instructions: ["string"]
-//         tags: ["string"]
-//         externalId: "string"
-//         totalNutrients: {}
-//         totalDaily: {}
-//         digest: [
-//           {
-//             label: "string"
-//             tag: "string"
-//             schemaOrgTag: "string"
-//             total: 0
-//             hasRDI: true
-//             daily: 0
-//             unit: "string"
-//             sub: {}
-//           }
-//         ]
-//       }
-//       _links: {
-//         self: {
-//           href: "string"
-//           title: "string"
-//         }
-//         next: {
-//           href: "string"
-//           title: "string"
-//         }
-//       }
-//     }
-//   ]
-// }
+import { MouseEvent, useState } from "react"
 
 export const Search = () => {
+  const [data, setData] = useState(null)
+
+  const fetchData = async () => {
+    try {
+      const query = "gluten free"
+      const appID = "cb42b4a6"
+      const appKey = "dfa5b9f944acbf6422e2d2e3c90649e2"
+      const url = `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${appKey}`
+
+      const response = await fetch(url)
+      const jsonData = await response.json()
+
+      setData(jsonData)
+      console.log(data)
+    } catch (error) {
+      console.error("Error fetching data:", error)
+    }
+  }
   const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    getRecipes()
+    fetchData()
   }
 
   const axios = require("axios")
