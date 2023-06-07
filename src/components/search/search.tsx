1ffdc3dc8e795033343bb7901cc6a261ba607819
Emma Fattori <emma@applydigital.co>
2023-06-06 20:37:21 -0400

    Adding more functionality for search, refactoring promise

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,7 +1,8 @@
-import { MouseEvent, useState } from "react"
+import { MouseEvent, useEffect, useState } from "react"
 
 export const Search = () => {
-  const [data, setData] = useState()
+  const [data, setData] = useState(Array<{}>)
+  const recipeArr = []
   const query = "gluten free"
   const APP_ID = "cb42b4a6"
   const APP_KEY = "dfa5b9f944acbf6422e2d2e3c90649e2"
@@ -9,14 +10,26 @@ export const Search = () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     )
+    if (!response.ok) {
+      throw new Error("Failed to fetch")
+    }
     const recipeData = await response.json()
-    setData(recipeData)
-    console.log(recipeData)
+    return recipeData
   }
-  const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
+  // useEffect(() => {
+
+  // }, [])
+
+  const handleSubmit = () => {
     getRecipes()
+      .then((fetchedRecipes) => {
+        setData(fetchedRecipes.hits)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
+  console.log(data)
 
   return (
     <div>
@@ -26,6 +39,11 @@ export const Search = () => {
       <button type="submit" onClick={handleSubmit}>
         Search
       </button>
+      <div className="results">
+        {data.map((recipe, idx) => (
+          <li key={idx}>{idx}</li>
+        ))}
+      </div>
     </div>
   )
 }
