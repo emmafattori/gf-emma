8cc718f10bd924f62271ad5d406ebbbb8305f5ad
Emma Fattori <emma@applydigital.co>
2023-08-22 17:45:42 -0400

    Setting up details page API call

--- src/pages/recipes/[index].tsx
+++ src/pages/recipes/[index].tsx
@@ -2,3 +2,22 @@ import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
 import { getRecipes } from "@/pages/api/getRecipes"
+type Recipe = {
+  label: string
+}
+const RecipeDetailPage = () => {
+  const router = useRouter()
+  const index = router.query
+
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+
+  useEffect(() => {}, [index])
+
+  const recipeIndex = typeof index === "string" ? parseInt(index, 10) : -1
+  const recipe =
+    recipeIndex >= 0 && recipeIndex < recipes.length
+      ? recipes[recipeIndex]
+      : null
+}
+
+export default RecipeDetailPage
