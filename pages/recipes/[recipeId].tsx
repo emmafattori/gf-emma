c92a843b15fcf527c729120fe48e853c2775830b
Emma Fattori <emmalfattori@gmail.com>
2023-11-20 21:30:24 -0500

    Updating styles on recipe details, adding breadcrumb

--- pages/recipes/[recipeId].tsx
+++ pages/recipes/[recipeId].tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { getRecipeDetails } from "../api/getRecipeDetails"
 import "../../src/globals.css"
 import Image from "next/image"
+import Link from "next/link"
 
 const RecipeDetailsPage = () => {
   const router = useRouter()
@@ -15,9 +16,19 @@ const RecipeDetailsPage = () => {
   console.log(recipe)
   return (
     <>
-      <div className="h-screen w-4/5 m-auto my-4">
+      <div className="w-4/5 m-auto my-4">
+        <Link href={"/"} className="flex">
+          <Image
+            src="/left-arrow.png"
+            alt=""
+            width={25}
+            height={25}
+            className="mx-5"
+          />{" "}
+          Back to home
+        </Link>
         <header className="text-center">
-          <h1 className="text-4xl">Recipe Details Page</h1>
+          <h1 className="text-4xl">{recipe.label}</h1>
         </header>
         <div className="details-container w-4/5 m-auto text-center flex flex-col justify-center items-center my-8">
           <Image
@@ -28,8 +39,8 @@ const RecipeDetailsPage = () => {
           />
           <h2>{recipe.label}</h2>
 
-          <h5>Ingredients</h5>
-          <ul>
+          <h5 className="text-lg font-semibold my-7">Ingredients</h5>
+          <ul className="bg-white w-3/4 mx-auto rounded-xl p-4 m-4">
             {recipe.ingredientLines.map((item, idx) => {
               return <li key={idx}>{item}</li>
             })}
